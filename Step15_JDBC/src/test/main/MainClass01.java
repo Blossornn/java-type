db21f13aefd76db69b7e9555b5b69b53b3bed123
youngseok <gys329@naver.com>
2025-07-02 15:17:51 +0900

    pc로 옮기기 위한 커

---
+++ Step15_JDBC/src/test/main/MainClass01.java
@@ -0,0 +1,14 @@
+package test.main;
+/*
+ *  Java DataBase Connectivity (JDBC)
+ *  
+ *  - Java 에서 DB 에 연결해서 INSERT, UPDATE, DELETE, SELECT 등의 작업을 할 수 있다
+ *  - 우리는 ORACLE DataBase 에 연결해서 이런 작업을 할 예정
+ *  - Oracle 연결 driver Class가 있어야 연결 가능
+ */
+
+public class MainClass01 {
+	public static void main(String[] args) {
+			
+	}
+}
