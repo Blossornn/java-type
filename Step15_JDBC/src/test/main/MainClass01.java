09ab47f2adbb9456a7e4ff9aa5abd44b62194c18
youngseok <gys329@naver.com>
2025-07-02 16:07:32 +0900

    added

--- Step15_JDBC/src/test/main/MainClass01.java
+++ Step15_JDBC/src/test/main/MainClass01.java
@@ -9,6 +9,8 @@ package test.main;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class MainClass01 {
 	public static void main(String[] args) {
@@ -25,6 +27,18 @@ public class MainClass01 {
 				//예외가 발생하지 않고 여기까지 실행순서가 내려오면 접속 성공이다.
 				System.out.println("Oracle DB 접속 성공");
 			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			try {
+				// 실행할 sql 문 미리 준비
+				String sql = "SELECT empno, ename, job, sal FROM emp ORDER BY empno ASC";
+				// sql 문 을 전달하면서 PreparedStatement 객체의 참조값 얻어내기
+				pstmt = conn.prepareStatement(sql);
+				// SELECT 문 실행 후 결과리절트객체
+				rs = pstmt.executeQuery();
+			}catch(Exception e) {
 				e.printStackTrace();
 			}
 	}
