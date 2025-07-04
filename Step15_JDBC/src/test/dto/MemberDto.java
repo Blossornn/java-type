ef6407e592dfa9e1a4d3687c75cc898876331736
youngseok <gys329@naver.com>
2025-07-04 14:35:26 +0900

    수정 기능 추가중입니듀

--- Step15_JDBC/src/test/dto/MemberDto.java
+++ Step15_JDBC/src/test/dto/MemberDto.java
@@ -1,3 +1,4 @@
+
 package test.dto;
 
 public class MemberDto {
--- Step15_JDBC/src/test/frame/MemberFrame.java
+++ Step15_JDBC/src/test/frame/MemberFrame.java
@@ -2,15 +2,21 @@ package test.frame;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 import test.dao.MemberDao;
 import test.dto.MemberDto;
@@ -18,6 +24,12 @@ import test.dto.MemberDto;
 public class MemberFrame extends JFrame {
 	// 필요한 필드 정의 하기
 	JTextField inputName, inputAddr;
+	// 표 형식으로 정보를 출력할 UI
+	JTable table;
+	// JTable 에 출력 할 data 를 공급 해 줄 모델 객체
+	DefaultTableModel model;
+	// Member Dao 를 자주 사용 할 ㅇㅖ정이라 미리 필드에 참조하기
+	MemberDao dao = new MemberDao();
 
 	// 생성자
 	public MemberFrame() {
@@ -31,6 +43,41 @@ public class MemberFrame extends JFrame {
 
 		// JButton
 		JButton insertBtn = new JButton("저장");
+		JButton deleteBtn = new JButton("삭제");
+		JButton updateBtn = new JButton("수정");
+
+		// 수정 버튼
+		updateBtn.addActionListener((e) -> {
+			int selectedRow = table.getSelectedRow();
+			if (selectedRow == -1) {
+				JOptionPane.showMessageDialog(this, "수정 할 회원을 선택 해 주세요");
+				return;
+			}
+			int num = (int) model.getValueAt(selectedRow, 0);
+			String name = inputName.getText();
+			String addr = inputAddr.getText();
+
+		});
+
+
+		// 삭제 버튼 눌렀을 때 실행 할 함수 등록
+		deleteBtn.addActionListener((e) -> {
+			// 선택 된 row 의 index 값을 읽어온다.
+			int selectedRow = table.getSelectedRow();
+			// 만일 선택 된 row 가 없다면
+			if (selectedRow == -1) {
+				JOptionPane.showMessageDialog(this, " 삭제 할 Row를 선택 해 주세요!");
+				return; // 메소드(함수) 를 여기서 끝내기 (리턴하기)
+			}
+			// 삭제 할 회원의 PRIMARY KEY 를 읽어와서
+			int num = (int) model.getValueAt(selectedRow, 0);
+			// DB에서 삭제
+			dao.delteteByNum(num);
+			// 테이블 리셋 후 DB에서 다시 불러오기
+			printMember();
+			
+		});
+		
 
 		// 패널에 UI 배치
 		JPanel panel = new JPanel();
@@ -39,10 +86,38 @@ public class MemberFrame extends JFrame {
 		panel.add(label2);
 		panel.add(inputAddr);
 		panel.add(insertBtn);
+		panel.add(deleteBtn);
+		panel.add(updateBtn);
 		// 패널의 배경 색 설정
 		panel.setBackground(Color.orange);
 		// 패널을 프레임의 위쪽에 배치
 		add(panel, BorderLayout.NORTH);
+
+		// 회원 목록을 출력 할 테이블
+		table = new JTable();
+		// 테이블의 칼럼 명을 배열로 미리 준비
+		String[] colNames = { "번호", "이름", "주소" };
+		// 테이블에 연결 할 모델 객체
+		model = new DefaultTableModel();
+		// 모델에 칼럼 명을 전달
+		model.setColumnIdentifiers(colNames);
+		model.setRowCount(0);
+		// 모델 명을 테이블에 연결
+		table.setModel(model);
+		// 테이블의 글자 크기와 행의 높이 조절
+		table.getTableHeader().setFont(new Font("Sans-serif", Font.BOLD, 18));
+		table.setFont(new Font("Sans-serif", Font.PLAIN, 16)); // 데이터 글자 크기 14
+		table.setRowHeight(25); // 각 행의 높이를 조정
+
+		// row 가 많은 경우 스크롤이 가능 하도록 scroll 기능 부여
+		JScrollPane scroll = new JScrollPane(table);
+		// 테이블에 품고 있는 JScrollPane 을 프레임의 가운데에 배치
+		add(scroll, BorderLayout.CENTER);
+
+		// sample data 를 table 에 출력하기
+//		Object[] row1 = { 0, "sample name", "sample addr" };
+//		model.addRow(row1);
+		printMember();
 
 		insertBtn.addActionListener(new ActionListener() {
 
@@ -58,12 +133,35 @@ public class MemberFrame extends JFrame {
 				boolean isSuccess = new MemberDao().insert(dto);
 
 				if (isSuccess) {
-					JOptionPane.showMessageDialog(MemberFrame.this, name + " 님의 정보가 DB에 저장되었습니다.");
+					// ✅ 아이콘 지정해서 듀크 대신 사용
+					ImageIcon icon = new ImageIcon("src/images/image_32x32.jpg");
+
+					// this 는 나의 참조값( 나 => MemberFrame 객체 => Component type 이기도 함
+					JOptionPane.showMessageDialog(MemberFrame.this, name + " 님의 정보가 저장되었습니다.", "저장 성공",
+							JOptionPane.INFORMATION_MESSAGE, icon);
+
+					inputName.setText("");
+					inputAddr.setText("");
+					inputName.requestFocus();
+					// 테이블 리셋 후 DB에서 다시 불러오기
+					printMember();
 				} else {
-					System.out.println("잠시 후 다시 시도해주세요");
+					JOptionPane.showMessageDialog(MemberFrame.this, "저장 실패!", "에러", JOptionPane.ERROR_MESSAGE);
+
 				}
 			}
 		});
+	}
+
+	// 회원 목록을 출력하는 메소드
+	public void printMember() {
+		model.setRowCount(0); // 기존 테이블 비우기
+		List<MemberDto> list = dao.selectAll(); // 새 데이터 불러오기
+		for (MemberDto tmp : list) {
+			Object[] row = { tmp.getNum(), tmp.getName(), tmp.getAddr() };
+			model.addRow(row); // 테이블에 추가
+		}
+		
 	}
 
 	public static void main(String[] args) {
